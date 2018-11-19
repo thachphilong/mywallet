5855f578beaf0f2bbb7663a7b3c1f48b608edc21
Tuyet Hua Kim <tuyethua@chuongduong.vn>
2018-11-19 13:49:04 +0700

    Enter

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,3 +16,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+Route::redirect('/','/mywallet-master/public/login');
