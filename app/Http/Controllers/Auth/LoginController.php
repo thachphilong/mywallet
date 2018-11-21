36be685b030b6ecd532d2500568fc606bd07bf85
Tuyet Hua Kim <tuyethua@chuongduong.vn>
2018-11-22 02:08:35 +0700

    Enter

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
 class LoginController extends Controller
 {
     /*
@@ -32,12 +34,17 @@ class LoginController extends Controller
      *
      * @return void
      */
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            // Authentication passed...
+            return redirect()->intended('dashboard');
+        }
+    }
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-    }
-    public function googlelogin()
-    {
-        
     }
 }
