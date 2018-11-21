8f83cdaadbebddf1365462a74556bbfaf30c960e
Thach Phi Long <thachphilong@gmail.com>
2018-11-21 16:44:26 +0700

    Enter

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/login', function () {
-    return view('login');
-});
-Route::get('/dashboard', function () {
-    return view('dashboard');
-});
+// Route::get('/login', function () {
+//     return view('login');
+// });
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// });
 Route::redirect('/','/mywallet-master/public/login');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
