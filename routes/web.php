2704b95dc8e7763b71ace6dd892b0d5bbdb9f3f5
Thach Phi Long <thachphilong@gmail.com>
2018-11-22 04:11:08 +0700

    Entẻ

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,9 @@ Route::get('/', function () {
 // Route::get('/login', function () {
 //     return view('login');
 // });
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
 Route::redirect('/','/mywallet-master/public/login');
 
 Auth::routes();
